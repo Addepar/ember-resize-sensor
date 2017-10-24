bd2bcd0a06a7d4bf8504ac8e833b75b7a4dc5ac4
Christopher Garrett <me@pzuraq.com>
2017-10-23 17:02:36 -0700

    wip

--- index.js
+++ index.js
@@ -2,5 +2,31 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-resize-sensor'
+  name: 'ember-resize-sensor',
+
+  options: {
+    nodeAssets: {
+      'css-element-queries': {
+        vendor: {
+          srcDir: 'src',
+          destDir: 'resize-sensor',
+          include: ['ResizeSensor.js']
+        }
+      }
+    }
+  },
+
+  included(parent) {
+    let app = parent;
+
+    while (!app.import) {
+      app = app.parent;
+    }
+
+    app.import('vendor/resize-sensor/ResizeSensor.js', {
+      using: [
+        { transformation: 'amd', as: 'resize-sensor' }
+      ]
+    });
+  }
 };
