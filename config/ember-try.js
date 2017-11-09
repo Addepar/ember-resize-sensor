59eb84a15e1f3bd706da60a2170e35041128749c
Lian Liu <lian.liu@addepar.com>
2017-11-09 12:29:58 -0800

    update ember cli

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,14 +1,53 @@
 /* eslint-env node */
 module.exports = {
+  useYarn: true,
   scenarios: [
+    {
+      name: 'ember-1.11',
+      bower: {
+        dependencies: {
+          'ember': '~1.11.0',
+          'ember-cli-shims': 'ember-cli/ember-cli-shims#0.0.3'
+        },
+        resolutions: {
+          'ember': '~1.11.0',
+          'ember-cli-shims': '0.0.3'
+        }
+      },
+      npm: {
+        devDependencies: {
+          'ember-cli-shims': null,
+          'ember-source': null
+        }
+      }
+    },
+    {
+      name: 'ember-1.13',
+      bower: {
+        dependencies: {
+          'ember': '~1.13.0',
+          'ember-cli-shims': '0.0.6'
+        },
+        resolutions: {
+          'ember': '~1.13.0',
+          'ember-cli-shims': '0.0.6'
+        }
+      },
+      npm: {
+        devDependencies: {
+          'ember-cli-shims': null,
+          'ember-source': null
+        }
+      }
+    },
     {
       name: 'ember-lts-2.4',
       bower: {
         dependencies: {
-          'ember': 'components/ember#lts-2-4'
+          'ember': 'components/ember#lts-2-4',
         },
         resolutions: {
-          'ember': 'lts-2-4'
+          'ember': 'lts-2-4',
         }
       },
       npm: {
@@ -21,15 +60,23 @@ module.exports = {
       name: 'ember-lts-2.8',
       bower: {
         dependencies: {
-          'ember': 'components/ember#lts-2-8'
+          'ember': 'components/ember#lts-2-8',
         },
         resolutions: {
-          'ember': 'lts-2-8'
+          'ember': 'lts-2-8',
         }
       },
       npm: {
         devDependencies: {
           'ember-source': null
+        }
+      }
+    },
+    {
+      name: 'ember-lts-2.12',
+      npm: {
+        devDependencies: {
+          'ember-source': '~2.12.0'
         }
       }
     },
