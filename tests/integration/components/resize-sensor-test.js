3b0bdbe015f94c487fe57e3e5e0d02ec6695b17e
Lian Liu <lian.liu@addepar.com>
2017-11-09 17:05:50 -0800

    test added

--- tests/integration/components/resize-sensor-test.js
+++ tests/integration/components/resize-sensor-test.js
@@ -1,5 +1,8 @@
+import { htmlSafe } from "@ember/string";
+import { later } from "@ember/runloop"
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import wait from 'ember-test-helpers/wait';
 
 moduleForComponent('resize-sensor', 'Integration | Component | resize sensor', {
   integration: true
@@ -9,6 +12,8 @@ test('it renders', function(assert) {
 
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
+
+  assert.expect(2);
 
   this.render(hbs`{{resize-sensor}}`);
 
@@ -23,3 +28,33 @@ test('it renders', function(assert) {
 
   assert.equal(this.$().text().trim(), 'template block text');
 });
+
+test('when the target changes sizes it will trigger the action', function(assert){
+  
+    assert.expect(1);
+  
+    this.set('style', htmlSafe('width: 500px; height: 500px;'));
+  
+    let updated;
+    this.on('resized', function(element){
+      updated = {
+        width: element.offsetWidth,
+        height: element.offsetHeight
+      }
+    });
+  
+    this.render(hbs`
+    <div style={{style}}>
+      {{#resize-sensor onResize='resized'}}
+      {{/resize-sensor}}
+    </div>
+    `);
+  
+    this.set('style', htmlSafe('width: 250px; height: 250px;'));
+  
+    later(() => {
+      assert.deepEqual(updated, { width: 250, height: 250 }, 'received updated size');
+    }, 70);
+  
+    return wait();
+  });
